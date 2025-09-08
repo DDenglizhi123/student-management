4085eea05b5cd0e4c54cdf6c0d86b8c0a69b76ee
DDenglizhi123 <128276398+DDenglizhi123@users.noreply.github.com>
2025-09-08 13:41:24 +0300

    init

---
+++ grades/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+from .views import GradeListView
+
+urlpatterns = [
+    path('',GradeListView.as_view(), name = 'grades_list'),
+]
---
+++ grades/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Grade
+# Create your views here.
+class GradeListView(ListView):
+    model = Grade
+    template_name = 'grades/grades_list.html'
+    fields = [
+        'grade_name',
+        'grade_number'
+    ]
