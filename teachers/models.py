d3468c4558923882be7f9b2ea820a8deecfee2d8
DDenglizhi123 <neverforever@vip.qq.com>
2025-09-22 13:33:54 +0300

    add teachers app

--- teachers/models.py
+++ teachers/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from grades.models import Grade
+
+
+
+GENDER_CHOICES = [
+    ('M', '男'),
+    ('F', '女'),
+]
 
 # Create your models here.
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='teacher')
+    teacher_name = models.CharField(max_length=50, verbose_name="老师姓名")
+    phone_number = models.CharField(max_length=11, unique=True, verbose_name="联系电话")
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name="性别")
+    birthday = models.DateField(help_text='yyyy-mm-dd', verbose_name="出生日期")
+    grade = models.ForeignKey(Grade, on_delete=models.DO_NOTHING, related_name='teachers', verbose_name="管理的班级")
+    
+    def __str__(self):
+        return self.teacher_name
+    
+    class Meta:
+        db_table = 'teacher'
+        verbose_name = '老师信息'
+        verbose_name_plural = '老师信息'
--- teachers/views.py
+++ teachers/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from .models import Teacher
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+
+
 
 # Create your views here.
+class BaseTeacherView():
+    model = Teacher
+    success_url = reverse_lazy("teacher_list")
+
+class TeacherListView(BaseTeacherView, ListView):
+    template_name = "teachers/teacher_list.html"
+    context_object_name = "teachers"
+    paginate_by = 10
+    ordering = ['id']
+class TeacherCreateView(BaseTeacherView, CreateView):
+    pass
+class TeacherUpdateView(BaseTeacherView, UpdateView):
+    pass
+class TeacherDeleteView(BaseTeacherView, DeleteView):
+    pass
