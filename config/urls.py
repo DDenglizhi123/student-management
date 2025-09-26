da94cca8e00aae8f12813690e77c63cb3d5d46ba
DDenglizhi123 <neverforever@vip.qq.com>
2025-09-26 07:59:01 +0300

    score

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from accounts import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("grades/", include("grades.urls")),
     path("students/", include("students.urls")),
     path("teachers/", include("teachers.urls")),
     path("scores/", include("scores.urls")),
+    path("login/", views.user_login, name="login"),
 ]
--- scores/urls.py
+++ scores/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import (ScoreListView, ScoreCreateView, ScoreUpdateView, ScoreDeleteView, ScoreDeleteMultipleView, ScoreDetailView, export_scores, upload_scores, MyScoreListView)
+from .views import upload_scores, export_scores, ScoreListView, ScoreCreateView, ScoreUpdateView, ScoreDeleteView,ScoreDeleteMultipleView, ScoreDetailView, MyScoreListView
+
 urlpatterns = [
-    path("", ScoreListView.as_view(), name="score_list"),
-    path("create/", ScoreCreateView.as_view(), name="score_create"),
-    path("<int:pk>/update/", ScoreUpdateView.as_view(), name="score_update"),
-    path("<int:pk>/delete/", ScoreDeleteView.as_view(), name="score_delete"),
-    path("delete_multiple/", ScoreDeleteMultipleView.as_view(), name="score_delete_multiple"),
-    path("detail/<int:pk>/", ScoreDetailView.as_view(), name="score_detail"),
-    path("upload_scores/", upload_scores, name="upload_scores"), # type: ignore
-    path("export_scores/", export_scores, name="export_scores"), # type: ignore
-    path("my_scores/", MyScoreListView.as_view(), name="my_scores"), # type: ignore
+    path('', ScoreListView.as_view(), name='score_list'),
+    path('create/', ScoreCreateView.as_view(), name='score_create'),
+    path('update/<int:pk>/', ScoreUpdateView.as_view(), name='score_update'),
+    path('delete/<int:pk>/', ScoreDeleteView.as_view(), name='score_delete'),
+    path('delete_multiple/', ScoreDeleteMultipleView.as_view(), name='score_delete_multiple'),
+    path('detail/<int:pk>/', ScoreDetailView.as_view(), name='score_detail'),
+    path('upload_scores/', upload_scores, name="upload_scores"),
+    path('export_scores/', export_scores, name='export_scores'),
+    path('my_scores', MyScoreListView.as_view(), name='my_scores'),
 ]
